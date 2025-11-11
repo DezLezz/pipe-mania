0b2eba4030c9c81c6f1d47960c5a682d38537f01
DezLezz <marcoantoniocabral2@gmail.com>
2025-11-11 21:36:54 +0000

    fix: remove unsued configs

--- src/game/types/GameConfig.tsx
+++ src/game/types/GameConfig.tsx
@@ -4,16 +4,13 @@ export interface GameConfig {
     startDelay: number
     segmentDelay: number
     pipePlacementDelay: number
-    speedMultiplier: number
     minRequiredLength: number
     maxRequiredLength: number
     maxWaterLevel: number
   }
   pieceSet: { type: string; enabled: boolean }[]
   rules: {
-    allowReplace: boolean
     minBlockedCells: number
     maxBlockedCells: number
-    randomStart: number
   }
 }
